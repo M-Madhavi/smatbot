8ef0d01d2fffc7a52cc847028179c9f5cce1a29a
M-Madhavi <62045841+M-Madhavi@users.noreply.github.com>
2021-11-09 15:00:01 +0530

    Update Contact.js

--- routes/Contact.js
+++ routes/Contact.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const checkAuthentication = require('../middleware/auth,js')
+const checkAuthentication = require('../middleware/auth.js')
 
 const {createContact,siginInContact,getContactById,deleteContactById,getAllContacts,updateContact} = require('../controllers/ContactController')
 
